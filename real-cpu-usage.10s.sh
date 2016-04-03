b9c81ab48a86a45e3da2434d6acc049295838638
Chris Metcalf <chris@chrismetcalf.net>
2016-04-03 14:28:41 -0600

    A CPU plugin we can believe in

---
+++ real-cpu-usage.10s.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# <bitbar.title>Real CPU Usage</bitbar.title>
+# <bitbar.author>Mat Ryer and Tyler Bunnell</bitbar.author>
+# <bitbar.author.github>matryer</bitbar.author.github>
+# <bitbar.desc>Calcualtes and displays real CPU usage stats.</bitbar.desc>
+
+IDLE=$(top -F -R -l3 | grep "CPU usage" | tail -1 | egrep -o '[0-9]{0,3}\.[0-9]{0,2}% idle' | sed 's/% idle//')
+
+USED=$(echo 100 - "$IDLE" | bc)
+
+echo "$USED%"
+echo "---"
+uptime
+echo "---"
+/usr/local/bin/gdf -h | tail -n -1
+echo "---"
+iostat
